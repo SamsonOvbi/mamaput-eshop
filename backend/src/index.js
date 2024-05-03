57aa66f70a045d53e4c3c6d61404d8439f642dd1
Samson Rerri <samson.rerri@gmail.com>
2024-05-03 20:43:20 +0100

    app.use(express.static(path.join(__dirname, './public')));

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,7 +38,7 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(express.static(path.join(__dirname, '../../frontend/dist/frontend')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
